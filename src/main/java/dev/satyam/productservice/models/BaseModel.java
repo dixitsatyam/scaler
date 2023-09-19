e6adeeb96cbff72cbc2d047a1894c0a48d539692
Satyam Dixit <satyam.dixit@mavenir.com>
2023-09-19 12:02:11 +0530

    connect to mysql database

--- src/main/java/dev/satyam/productservice/models/BaseModel.java
+++ src/main/java/dev/satyam/productservice/models/BaseModel.java
@@ -1,5 +1,10 @@
 package dev.satyam.productservice.models;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseModel {
+    @Id
     private long id;
 }
